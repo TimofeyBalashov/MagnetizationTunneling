24962a7c5e43e666db0ee5e7fe610b1fb942c850
TimofeyBalashov <Timofey.Balashov@kit.edu>
2017-06-06 15:35:47 +0200

    Cleaned up library

--- pyatoms/J/JSystem.py
+++ pyatoms/J/JSystem.py
@@ -29,20 +29,16 @@ class JSystem(QuantumSystem):
         return np.real(np.array(self._Js.tolist(), dtype=complex).flatten())
 
     @resultmethod
-    def J_transitions(self, power=1, N=None):
+    def J_transitions(self, N=None):
         def multiply(m1,m2):
             return mp.matrix(np.asarray(m1.tolist())*np.asarray(m2.tolist()))
 
-        if power == 0:
-            T = self.transitions(mp.matrix(np.eye(self.nstates)))[0]
-            return multiply(T,T.conjugate())[:N, :N]
-        
         JZ, JP, JM = self.transitions(self.Jz, self.Jp, self.Jm)
 
         if N is None or N > self.nstates:
             N = self.nstates
 
-        return (((2*multiply(JZ,JZ.conjugate()) +
+        return ((2*multiply(JZ,JZ.conjugate()) +
                 multiply(JP,JP.conjugate()) +
-                multiply(JM,JM.conjugate()))/2/self.J/(self.J+1))**power)[:N, :N]
-        
+                multiply(JM,JM.conjugate()))/2/self.J/(self.J+1))[:N, :N]
+
